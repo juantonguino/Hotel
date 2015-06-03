a9d01b3f0ae0462143da122aafae269e7545e237
andres97473 <andres97473@hotmail.com>
2015-06-02 22:45:33 -0500

    cambio ojeda 13 se incluyo el checkout

--- vistas/opciones.php
+++ vistas/opciones.php
@@ -14,12 +14,13 @@ $numero=$_GET['numero'];
 <table>
 	<thead>
 		<tr>
-			<th colspan="3"> HOTEL U-MARIANA </th>
+			<th colspan="4"> HOTEL U-MARIANA </th>
 		</tr>
 		<tr>
                     <td><a href="<?php echo "huespedes.php?numero=".$numero;?>"><img src="../img/huespedes.jpg" height="100" width="100"></a></td>
                     <td><a href="<?php echo "consumos.php?numero=".$numero;?>"><img src="../img/consumos.jpg" height="100" width="100"></a></td>
                     <td><a href="<?php echo "reservas.php?numero=".$numero;?>"><img src="../img/reservas.jpg" height="100" width="100"></a></td>
+                    <td><a href="<?php echo "checkout.php?numero=".$numero;?>"><img src="../img/checkout.png" height="100" width="100"></a></td>
 			
 		</tr>
 	</thead>
@@ -28,9 +29,10 @@ $numero=$_GET['numero'];
 			<td><a href="<?php echo "huespedes.php?numero=".$numero;?>">Huespedes</a></td>
                         <td><a href="<?php echo "consumos.php?numero=".$numero;?>">Consumos</a></td>
                         <td><a href="<?php echo "reservas.php?numero=".$numero;?>">Reservas</a></td>
+                        <td><a href="<?php echo "checkout.php?numero=".$numero;?>">check-out</a></td>
 		</tr>
 		<tr>
-                    <td colspan="3"><h6><a href="login.php">Regresar</a></h6></td>
+                    <td colspan="4"><h6><a href="login.php">Regresar</a></h6></td>
 		</tr>
 	</tbody>
 </table>
